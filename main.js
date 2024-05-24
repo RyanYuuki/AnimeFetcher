a3b85d7ed2972ec4dc40d03776f51b06ea154650
Ryan _ <108048963+RyanYuuki@users.noreply.github.com>
2024-05-24 22:44:03 +0530

    - Fixed Logic

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ function fetchType(Type)
         fetchTypeElmt.children[2].classList.remove('Active');
         fetchTypeElmt.children[0].classList.add('Active');
         document.getElementsByClassName("inputBox")[0].style.display = 'none'; 
-        searchAnime(0)
+        searchAnime(0);
     }
-    else if(Type == 'Anime')
+    if(Type == 'Anime')
     {
         fetchTypeElmt.children[0].classList.remove('Active');
         fetchTypeElmt.children[2].classList.remove('Active');
@@ -18,7 +18,7 @@ function fetchType(Type)
         document.getElementsByClassName("inputBox")[0].style.display = 'grid'; 
         searchType = 1;
     }
-    else if(Type == 'Character')
+    if(Type == 'Character')
     {
         fetchTypeElmt.children[1].classList.remove('Active');
         fetchTypeElmt.children[0].classList.remove('Active');
@@ -27,23 +27,29 @@ function fetchType(Type)
         searchType = 2;
     }
 }
-function searchAnime(searchType)
+async function searchAnime(searchType)
 {
     const searchBarElmt = document.getElementsByClassName("searchBar")[0];
     const quoteElmt = document.getElementsByClassName("Quote")[0];
+    const referenceElmt = document.getElementsByClassName("Reference")[0]; 
     if(searchType == 0)
     {
-        fetch("https://animechan.xyz/api/random")
-        .then((response) => response.json())
-        .then(quote => quoteElmt.textContent = `${quote.quote}`)
+        const Response = await fetch("https://animechan.xyz/api/random");
+        const Data = await  Response.json();
+        quoteElmt.textContent = `${Data.quote}`;
+        referenceElmt.textContent = `- ${Data.character}`;
     }
-    else if (searchType == 1) {
-        fetch(`https://animechan.xyz/api/random/anime?title=${searchBarElmt.value}`)
-        .then(response => response.json())
-        .then(quote => quoteElmt.textContent = `${quote.quote}`)
-    } else if (searchType == 2) {
-        fetch(`https://animechan.xyz/api/random/character?name=${searchBarElmt.value}`)
-        .then((response) => response.json())
-        .then((quote) => quoteElmt.textContent = `${quote.quote}`)        
-    }   
+    if (searchType == 1) {
+        const Response = await fetch("https://animechan.xyz/api/random/anime?title=" + searchBarElmt.value);
+        const Data = await Response.json();
+        quoteElmt.textContent = `${Data.quote}`
+        referenceElmt.textContent = `- ${Data.character}`;
+    } 
+    if (searchType == 2) {
+        const Response = await fetch(`https://animechan.xyz/api/random/character?name=${searchBarElmt.value}`)
+        const Data = await Response.json();
+        quoteElmt.textContent = `${Data.quote}`;
+        referenceElmt.textContent = `- ${Data.anime}`;        
+    }
+
 }
